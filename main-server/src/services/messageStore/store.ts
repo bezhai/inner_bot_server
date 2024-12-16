6989d823e65ae5c6608f67d06992b25654e26192
yuanzhihong.chiwei <yuanzhihong.chiwei@bytedance.com>
2024-12-17 03:14:11 +0800

    修一下

--- main-server/src/services/messageStore/store.ts
+++ main-server/src/services/messageStore/store.ts
@@ -2,26 +2,37 @@ import { getMessageCollection } from "../../mongo/client";
 import { LarkMessageMetaInfo } from "../../types/mongo";
 
 export async function saveMessage(message: LarkMessageMetaInfo) {
-    const collection = await getMessageCollection();
-    return collection.insertOne(message);
+  const collection = await getMessageCollection();
+  return collection.insertOne(message);
 }
 
 export async function getMessage(messageId: string) {
-    const collection = await getMessageCollection();
-    return collection.findOne({ message_id: messageId });
+  const collection = await getMessageCollection();
+  return collection.findOne({ message_id: messageId });
 }
 
 export async function recallMessage(messageId: string) {
-    const collection = await getMessageCollection();
-    return collection.updateOne({ message_id: messageId }, { is_delete: true });
+  const collection = await getMessageCollection();
+  return collection.updateOne({ message_id: messageId }, { is_delete: true });
 }
 
 export async function updateRobotMessageText(messageId: string, text: string) {
-    const collection = await getMessageCollection();
-    return collection.updateOne({ message_id: messageId }, { robot_text: text });
+  const collection = await getMessageCollection();
+  return collection.updateOne({ message_id: messageId }, { robot_text: text });
 }
 
-export async function searchMessageByRootId(rootId: string, messageType = "text", limit = 6) {
-    const collection = await getMessageCollection();
-    return collection.find({ root_id: rootId, message_type: messageType, is_delete: false }, { sort: { create_time: -1 }, limit });
+export async function searchMessageByRootId(
+  rootId: string,
+  messageType = "text",
+  limit = 6
+) {
+  const collection = await getMessageCollection();
+  return collection.find(
+    {
+      root_id: rootId,
+      $and: [{ message_type: messageType }, { is_from_robot: true }],
+      is_delete: false,
+    },
+    { sort: { create_time: -1 }, limit }
+  );
 }
