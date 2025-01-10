477cb129a3a7a7c91dd44ff9872437fa2269b591
yuanzhihong.chiwei <yuanzhihong.chiwei@bytedance.com>
2025-01-11 02:14:26 +0800

    发图时增加按钮

--- main-server/src/services/chat/rules/send-photo.ts
+++ main-server/src/services/chat/rules/send-photo.ts
@@ -2,13 +2,26 @@ import dayjs from "dayjs";
 import { StatusMode } from "../../../types/pixiv";
 import { getPixivImages, uploadToLark } from "../../pixivProxy/proxy";
 import { calcBestChunks } from "../../../utils/calc-photo";
-import { LarkCard, CardHeader, ColumnSet, ImgComponent, Column } from "feishu-card";
+import {
+  LarkCard,
+  CardHeader,
+  ColumnSet,
+  ImgComponent,
+  Column,
+  ActionComponent,
+  ButtonComponent,
+} from "feishu-card";
 import { replyCard, replyMessage } from "../../larkBasic/message";
 import { CommonMessage } from "../../../models/common-message";
 
 export async function sendPhoto(message: CommonMessage) {
   try {
-    const tags = message.clearText().replace(/^发/, "").trim().split(/\s+/).filter((tag) => tag.length > 0);
+    const tags = message
+      .clearText()
+      .replace(/^发/, "")
+      .trim()
+      .split(/\s+/)
+      .filter((tag) => tag.length > 0);
     if (tags.length <= 0) {
       throw new Error("标签格式错误");
     }
@@ -19,7 +32,6 @@ export async function sendPhoto(message: CommonMessage) {
     replyMessage(message.messageId, `发送图片失败: ${errorMessage}`, true);
   }
 }
-
 
 async function searchAndSendPhoto(tags: string[], messageId: string) {
   let images = await getPixivImages({
@@ -50,30 +62,43 @@ async function searchAndSendPhoto(tags: string[], messageId: string) {
 
   const { chunks, weights } = calcBestChunks(images);
 
-  const card = new LarkCard(
-    new CardHeader("")
-  ).addElements(
-    new ColumnSet()
-      .setHorizontalSpacing("small")
-      .addColumn(
-        new Column()
-          .setWidth("weighted", weights[0])
-          .addElements(
-            ...chunks[0].map(
-              (image) => new ImgComponent(image.image_key!, image.pixiv_addr)
+  const card = new LarkCard()
+    .addElements(
+      new ColumnSet()
+        .setHorizontalSpacing("small")
+        .addColumn(
+          new Column()
+            .setWidth("weighted", weights[0])
+            .addElements(
+              ...chunks[0].map(
+                (image) => new ImgComponent(image.image_key!, image.pixiv_addr)
+              )
             )
-          )
-      )
-      .addColumn(
-        new Column()
-          .setWidth("weighted", weights[1])
-          .addElements(
-            ...chunks[1].map(
-              (image) => new ImgComponent(image.image_key!, image.pixiv_addr)
+        )
+        .addColumn(
+          new Column()
+            .setWidth("weighted", weights[1])
+            .addElements(
+              ...chunks[1].map(
+                (image) => new ImgComponent(image.image_key!, image.pixiv_addr)
+              )
             )
-          )
+        )
+    )
+    .addElements(
+      new ActionComponent().addActions(
+        new ButtonComponent().setText("换一批").addCallbackBehavior({
+          type: "send-photo",
+          tags,
+          chat_id: messageId,
+        }),
+        new ButtonComponent().setText("查看详情").addCallbackBehavior({
+          type: "send-photo-info",
+          images: images.map((image) => image.pixiv_addr),
+          chat_id: messageId,
+        })
       )
-  );
+    );
 
   await replyCard(messageId, card);
 }
