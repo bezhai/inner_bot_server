12af96c3345ab66eda4582693d69d4331fb3be22
yuanzhihong.chiwei <yuanzhihong.chiwei@bytedance.com>
2025-03-18 15:35:02 +0800

    增加提示

--- main-server/src/services/callback/set-llm-config.ts
+++ main-server/src/services/callback/set-llm-config.ts
@@ -1,6 +1,8 @@
 import AppDataSource from '../../ormconfig';
 import { ChatModelMapping, ChatPromptMapping } from '../../dal/entities';
 import { LarkCallbackInfo, SetLLMConfigFormValue } from '../../types/lark';
+import { replyMessage } from '../lark/basic/message';
+import { AIModelRepository, AIPromptRepository } from '../../dal/repositories/repositories';
 
 export async function handleSetLLMConfig(data: LarkCallbackInfo, fromValue: SetLLMConfigFormValue) {
   const { select_model, select_prompt } = fromValue;
@@ -18,4 +20,18 @@ export async function handleSetLLMConfig(data: LarkCallbackInfo, fromValue: SetL
       .getRepository(ChatPromptMapping)
       .upsert({ chat_id: chatId, prompt_id: select_prompt }, { conflictPaths: ['chat_id'] });
   });
+
+  const model = await AIModelRepository.findOne({
+    where: {
+      model_id: select_model,
+    },
+  });
+
+  const prompt = await AIPromptRepository.findOne({
+    where: {
+      prompt_id: select_prompt,
+    },
+  });
+
+  replyMessage(data.context.open_message_id, `设置模型[${model?.name}]和提示词[${prompt?.name}]成功`);
 }
