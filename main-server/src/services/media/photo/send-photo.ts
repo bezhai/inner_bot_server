496ba3b3e180143cb9e32f5410b10ee5f7f7775a
yuanzhihong.chiwei <yuanzhihong.chiwei@bytedance.com>
2025-05-26 11:34:22 +0800

    限制大群发图

--- main-server/src/services/media/photo/send-photo.ts
+++ main-server/src/services/media/photo/send-photo.ts
@@ -1,3 +1,4 @@
+import { GroupChatInfoRepository } from '../../../dal/repositories/repositories';
 import { Message } from '../../../models/message';
 import { replyCard, replyMessage } from '../../lark/basic/message';
 import { searchAndBuildPhotoCard } from './photo-card';
@@ -11,7 +12,31 @@ export async function sendPhoto(message: Message) {
             .split(/\s+/)
             .filter((tag) => tag.length > 0);
         if (tags.length <= 0) {
-            throw new Error('标签格式错误');
+            throw new Error('呜呜~要发图的话，记得带上标签告诉人家想看什么嘛(｡•́︿•̀｡)');
+        }
+
+        const groupInfo =
+            message.basicChatInfo?.chat_mode === 'group'
+                ? await GroupChatInfoRepository.findOne({
+                      where: {
+                          chat_id: message.chatId,
+                      },
+                  })
+                : null;
+
+        // 检查是否允许发送图片，满足以下任意一个条件
+        // 1. 单聊
+        // 2. 群聊，且人数<=20
+        // 3. 群聊，且开白名单
+        const allowSendPhoto =
+            message.basicChatInfo?.chat_mode === 'p2p' ||
+            (groupInfo && groupInfo.user_count <= 20) ||
+            message.basicChatInfo?.permission_config?.allow_send_message;
+
+        if (!allowSendPhoto) {
+            throw new Error(
+                '诶嘿~这个群人有点多呢，发图功能暂时关闭啦(｡•́︿•̀｡) 想用的话可以联系开发者主人帮忙开白哦！',
+            );
         }
 
         const photoCard = await searchAndBuildPhotoCard(
@@ -21,8 +46,11 @@ export async function sendPhoto(message: Message) {
 
         await replyCard(message.messageId, photoCard);
     } catch (e) {
-        const errorMessage = e instanceof Error ? e.message : '未知错误';
+        const errorMessage =
+            e instanceof Error
+                ? e.message
+                : '呜呜...好像遇到奇怪的小问题了呢 (´;ω;｀) 要不稍后再试试？';
         console.error(e);
-        replyMessage(message.messageId, `发送图片失败: ${errorMessage}`, true);
+        replyMessage(message.messageId, `呜哇，发图失败啦：${errorMessage}`, true);
     }
 }
