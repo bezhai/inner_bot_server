9ed7950f4cc550af682864554dc3f5f89ceaf1f9
yuanzhihong.chiwei <yuanzhihong.chiwei@bytedance.com>
2025-09-15 00:45:23 +0800

    1

--- main-server/src/services/integrations/memory.ts
+++ main-server/src/services/integrations/memory.ts
@@ -2,7 +2,7 @@ import http from '../../services/http';
 import { ChatMessage } from 'types/chat';
 import { AxiosError } from 'axios';
 
-const BASE_URL = `${process.env.AI_SERVER_HOST}:${process.env.AI_SERVER_PORT}`;
+const BASE_URL = `http://${process.env.AI_SERVER_HOST}:${process.env.AI_SERVER_PORT}`;
 
 export async function storeMessage(message: ChatMessage): Promise<void> {
     try {
