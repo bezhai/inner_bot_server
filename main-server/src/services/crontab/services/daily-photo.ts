68c7bc8e59aa1b1efca2c132a9ea8ac424f6ba92
yuanzhihong.chiwei <yuanzhihong.chiwei@bytedance.com>
2025-12-06 22:16:22 +0800

    消除重复逻辑

--- main-server/src/services/crontab/services/daily-photo.ts
+++ main-server/src/services/crontab/services/daily-photo.ts
@@ -1,18 +1,14 @@
 import { StatusMode } from 'types/pixiv';
 import dayjs from 'dayjs';
 import {
-    ButtonComponent,
     CardHeader,
-    Column,
-    ColumnSet,
     ImgComponent,
     LarkCard,
 } from 'feishu-card';
 import { fetchUploadedImages } from '@media/photo/upload';
 import { sendCard } from '@lark-basic/message';
-import { calcBestChunks } from '@media/photo/calc-photo';
-import { FetchPhotoDetails, UpdateDailyPhotoCard } from 'types/lark';
 import { Crontab, registerCrontabService } from '../decorators';
+import { searchAndBuildDailyPhotoCard } from '@media/photo/photo-card';
 
 export class DailyPhotoService {
     /**
@@ -45,58 +41,14 @@ export class DailyPhotoService {
      */
     @Crontab('30 19 * * *', { taskName: 'daily-new-photo', botName: 'bytedance' })
     async dailySendNewPhoto(): Promise<void> {
-        let images = await fetchUploadedImages({
-            status: StatusMode.NOT_DELETE,
-            page: 1,
-            page_size: 6,
-            random_mode: true,
-            start_time: dayjs().add(-1, 'day').valueOf(),
-        });
-
-        if (images.length <= 0) {
-            return;
-        }
-
-        const { chunks, weights } = calcBestChunks(images);
-
-        const card = new LarkCard()
-            .withHeader(new CardHeader('今日新图').color('green'))
-            .addElement(
-                new ColumnSet()
-                    .setHorizontalSpacing('small')
-                    .addColumns(
-                        new Column()
-                            .setWidth('weighted', weights[0])
-                            .addElements(
-                                ...chunks[0].map((image) =>
-                                    new ImgComponent(image.image_key!).setAlt(image.pixiv_addr),
-                                ),
-                            ),
-                        new Column()
-                            .setWidth('weighted', weights[1])
-                            .addElements(
-                                ...chunks[1].map((image) =>
-                                    new ImgComponent(image.image_key!).setAlt(image.pixiv_addr),
-                                ),
-                            ),
-                    ),
-                new ColumnSet().addColumns(
-                    new Column().addElements(
-                        new ButtonComponent().setText('换一批').addValue({
-                            type: UpdateDailyPhotoCard,
-                            start_time: dayjs().add(-1, 'day').valueOf(),
-                        }),
-                    ),
-                    new Column().addElements(
-                        new ButtonComponent().setText('查看详情').addValue({
-                            type: FetchPhotoDetails,
-                            images: images.map((image) => image.pixiv_addr),
-                        }),
-                    ),
-                ),
-            );
-
-        sendCard('oc_a79ce7cc8cc4afdcfd519532d0a917f5', card);
+    try {
+          const card = await searchAndBuildDailyPhotoCard(
+              dayjs().add(-1, 'day').valueOf()
+          );
+          sendCard('oc_a79ce7cc8cc4afdcfd519532d0a917f5', card);
+      } catch (e) {
+          console.error('daily send new photo error:', e);
+      }
     }
 }
 
